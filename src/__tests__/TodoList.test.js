c3ec40b661264912cf35e8cc4620ddbd01da2e48
OtsoLap <otsolappalainen@gmail.com>
2022-01-26 19:42:27 +0200

    todolist tests draft 1 done

--- src/__tests__/TodoList.test.js
+++ src/__tests__/TodoList.test.js
@@ -1,5 +1,29 @@
 import React from 'react';
-import '@testing-ibrary/jest-dom';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import TodoList from '../Components/TodoList'
-import mockData from './mockData'
+import mockData from '../util/mockData'
+
+describe('todo list test', () => {
+    let todoList
+
+    beforeEach(() => {
+        todoList = render(<TodoList todos={mockData} />)
+    })
+
+    it('should find the tasks', () => {
+        mockData.forEach((test) => expect(screen.getByText(test.task)).toBeInTheDocument());
+    });
+
+    it('should describe Task two by its content', () => {
+        expect(screen.queryByText(/Call Alice/i)).toBeInTheDocument();
+    })
+
+    it('should not find this fake task', () => {
+        expect(screen.queryByText(/Learn Context API/i)).not.toBeInTheDocument();
+    })
+
+    it('should find 5 default Todos from the mockup Data', () => {
+        expect(mockData.length).toBe(5);
+    })
+});
---
+++ src/util/mockData.js
@@ -0,0 +1,9 @@
+const mockData = [
+    { id: 1, task: 'Go to the supermarket', complete: false },
+    { id: 2, task: 'Call Alice', complete: false },
+    { id: 3, task: 'Ask Alice to call Bob', complete: false },
+    { id: 4, task: 'Do the dishes', complete: false },
+    { id: 5, task: 'Change car tyres', complete: false }
+]
+
+export default mockData;
